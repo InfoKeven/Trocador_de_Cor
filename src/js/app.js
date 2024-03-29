42819eafe7bef07b314e1c8cd25ba0e8633b81cf
Keven Pereira <infokeven37@gmail.com>
2024-03-29 17:13:15 -0300

    ajuste
    Ajustando o app js e ambientando o código no arquivo style css

--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,14 @@ const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
-btn.addEventListener("click", function() {
+btn.addEventListener("click", function () {
     const randomNumber = getRandomNumber();
 
     document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
 
 });
 
-function getRandomNumber () {
+function getRandomNumber() {
     return Math.floor(Math.random() * colors.length);
 }
